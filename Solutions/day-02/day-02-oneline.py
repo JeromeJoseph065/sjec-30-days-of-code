f31a13462efc210ee5b865358de370bcc9fbbfe3
JeromeJoseph065 <121025091+JeromeJoseph065@users.noreply.github.com>
2022-12-21 19:28:10 +0530

    Update day-02-oneline.py

--- Solutions/day-02/day-02-oneline.py
+++ Solutions/day-02/day-02-oneline.py
@@ -1,2 +1,2 @@
-# add a new line at the end of the input ( for the correct format of output )
+# add a new line at the end of the input ( for the correct output format )
 [print(sorted(list(map(int, input().split())))[tc%3]) for tc in range(int(input()))] 
