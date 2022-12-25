e7ff6060d78d23b8acbdaeca7f4c90302ca29aeb
JeromeJoseph065 <121025091+JeromeJoseph065@users.noreply.github.com>
2022-12-25 20:15:44 +0530

    Corrected

--- Solutions/day-04/day-04.cpp
+++ Solutions/day-04/day-04.cpp
@@ -6,7 +6,7 @@ typedef long long ll;
 int main(){
   long long n, m;
   cin >> n >> m;
-  if(!(n%3) && !(m%2) || !(n%2) && !(m%3)) cout << "YES" << endl;
+  if(!(n%3%2) && !(m%2)||!(m%3%2)&&!(n%2)) cout << "YES" << endl;
   else cout << "NO" << endl;
   return 0;
 }
