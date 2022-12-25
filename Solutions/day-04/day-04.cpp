cd401f4bc2fe5bc994b2084d49b530a76c228b30
JeromeJoseph065 <121025091+JeromeJoseph065@users.noreply.github.com>
2022-12-25 21:56:58 +0530

    Probably the last update

--- Solutions/day-04/day-04.cpp
+++ Solutions/day-04/day-04.cpp
@@ -6,7 +6,7 @@ typedef long long ll;
 int main(){
   long long n, m;
   cin >> n >> m;
-  if(!(n%3%2) && !(m%2)||!(m%3%2)&&!(n%2)) cout << "YES" << endl;
+  if(!((m*n)%6) && n > 1 && m > 1) cout << "YES" << endl;
   else cout << "NO" << endl;
   return 0;
 }
