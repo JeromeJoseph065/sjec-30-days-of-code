446551567be7bcdc713d76011f7b3eebd9f2cb18
JeromeJoseph065 <121025091+JeromeJoseph065@users.noreply.github.com>
2022-12-30 17:55:21 +0530

    day 8 done

---
+++ Solutions/day-08/day-08.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+bool isPrime(ll num){
+    if(num < 2) return false; // 0 and 1 arent primes
+    else if(num == 2) return true;
+    else if(!(num & 1)) return false;
+
+    else{
+        for(ll d = 3; d*d <= num; d += 2){
+            if(num % d == 0)
+                return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    ll m , n; cin >> m >> n;
+    while(!isPrime(m)) m++;
+
+    ll lastPrime = m;
+    for(ll i = lastPrime + 1; i <= n; i++){
+        if(isPrime(i)){
+            printf("%lld - %lld : %lld\n", lastPrime, i, i - lastPrime - 1);
+            lastPrime = i;
+        }
+    }
+
+    return 0;
+}
