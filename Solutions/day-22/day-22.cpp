89f12c7d51002a4c3945a37d8a58ac88cb4fca49
JeromeJoseph065 <121025091+JeromeJoseph065@users.noreply.github.com>
2023-01-10 00:14:39 +0530

    day-22 (dp) complete

---
+++ Solutions/day-22/day-22.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+map<ll, ll> dp;
+
+ll collatz(ll num){
+    if(num == 1) return 0;
+    else if(dp[num] != 0) return dp[num];
+    else if(num & 1) dp[num] = 1 + collatz(3 * num + 1);
+    else dp[num] = 1 + collatz(num / 2);
+
+    return dp[num];
+}
+
+int main(){
+    ll a, b, longest = 0, val = 0;
+    cin >> a >> b;
+    for(; a <= b; a++){
+        if(collatz(a) > longest){
+            longest = dp[a];
+            val = a;
+        }
+    }
+    cout << val << endl;
+}
